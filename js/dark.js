e16442cee9db73b450d864a992b9748e3a8e364a
dev2919 <dev2919@gmail.com>
2020-04-02 20:26:49 +0530

    dark ui added

---
+++ js/dark.js
@@ -0,0 +1,44 @@
+let i=true;
+console.log(document.getElementById("hey").href);
+
+let dark = () =>{
+  console.log(document.getElementById("body_bg"));
+
+  
+
+
+  if(i==true)  
+  {
+    i=false;
+    document.getElementById("hey").href = "http://127.0.0.1:5500/css/darkcol.css";
+    document.getElementById("shpd").src = "http://127.0.0.1:5500/shop2.png";
+    document.getElementById('y').textContent='🌙';
+    // document.getElementById("body_bg").style.backgroundColor = "#121212";
+    // document.getElementById("body_bg").style.color = "white";
+    
+
+    // let boxes = document.getElementsByClassName("card");
+    // console.log(boxes);
+    
+    // for(let i=0 ; i<5 ; i++){
+    //     boxes[i].style.backgroundColor = "#272727"; 
+    //     document.getElementsByClassName("card-body")[i].style.backgroundColor = "#272727";
+    //     document.getElementsByClassName("card-body")[i].style.backgroundColor = "#272727";
+    //     document.getElementsByClassName("role")[i].style.backgroundColor = "#272727";
+
+    //     boxes[i].style.boxShadow = "0px 0px 24px -5px rgb(0, 0, 0)"
+
+
+
+    }
+  else if(i == false){
+    // document.getElementById("body_bg").style.backgroundColor = "#F1F1F1";
+    // document.getElementById("body_bg").style.color = "black";
+    i=true;
+    document.getElementById("hey").href = "http://127.0.0.1:5500/css/colors.css";
+    document.getElementById("shpd").src = "http://127.0.0.1:5500/shop.png";
+    document.getElementById('y').textContent='☀️';
+
+  }
+  return i;
+}
