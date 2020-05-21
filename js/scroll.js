8836e56423c2aa3ecffcc8b1b2c3396adfef73e2
dev2919 <dev2919@gmail.com>
2020-05-22 03:17:43 +0530

    Responsive UI Changes

--- js/scroll.js
+++ js/scroll.js
@@ -2,6 +2,7 @@ let shad = () =>{
   let nav = document.getElementById("navu");
   let img = document.getElementsByClassName("vst")[0];
   let mybutton = document.getElementById("myBtn");
+  let Darkbutton = document.getElementById("drk");
 
   if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
     nav.style.boxShadow = "rgba(0, 0, 0, 0.62) 0px 9px 19px -19px";
@@ -37,8 +38,11 @@ let shad = () =>{
  
     if (document.body.scrollTop > 3520 || document.documentElement.scrollTop > 3520) {
       mybutton.style.display = "block";
+      Darkbutton.style.display = "none"
     } else {
       mybutton.style.display = "none";
+      Darkbutton.style.display = "block"
+
     }
 
 
@@ -55,10 +59,15 @@ let shad = () =>{
 let hght = () =>{
   let a = document.getElementsByClassName("main1")[0];
   a.classList.remove("temp");
+}
+
+setTimeout(hght, 3000);
+
+let hght2 = () =>{
   document.getElementById("body_bg").style.overflowY="visible";
 }
 
-setTimeout(hght, 4500);
+setTimeout(hght2, 4500);
 
 
             //Get the button
